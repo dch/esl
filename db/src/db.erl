55ccd595546030a9420babee428ce171a99bd681
Dave Cottlehuber <dch@skunkwerks.at>
2014-11-29 17:54:45 +0100

    db: destroy/1

--- db/src/db.erl
+++ db/src/db.erl
@@ -3,16 +3,18 @@
 -opaque item() :: {atom(), atom()}.
 -opaque db() :: list( item()).
 -export_type([item/0, db/0]).
--export[
-        new/0
+-export[ new/0,
+         destroy/1
        ].
 
+%% db:new() ⇒ Db.
 -spec new() -> db().
 new() -> [].
 
-
-%% db:new() ⇒ Db.
 %% db:destroy(Db) ⇒ ok.
+-spec destroy(db()) -> ok.
+destroy(_) -> ok.
+
 %% db:write(Key, Element, Db) ⇒ NewDb. db:delete(Key, Db) ⇒ NewDb.
 %% db:read(Key, Db) ⇒{ok, Element} | {error, instance}. db:match(Element, Db) ⇒
 %% [Key1, ..., KeyN].
