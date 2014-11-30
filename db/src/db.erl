1188db66e8be8ad1ce7c50cfe3b19a82910c7ada
Dave Cottlehuber <dch@skunkwerks.at>
2014-11-30 15:21:54 +0100

    db: use ETS

--- db/src/db.erl
+++ db/src/db.erl
@@ -10,78 +10,44 @@
 %-opaque db() :: list( key()).
 %-export_type([key/0, db/0]).
 -export[ new/0,
-         read/2,
-         write/3,
-         delete/2,
-         match/2,
-         destroy/1
+         read/1,
+         write/2,
+         delete/1,
+         match/1,
+         destroy/0
        ].
 
 %% db:new() ⇒ Db.
 % -spec new() -> db().
-new() -> [].
+new() ->
+    ets:new(db, [named_table, {keypos, 2}]).
 
 %% db:destroy(Db) ⇒ ok.
 % -spec destroy(db()) -> ok.
-destroy(_) -> ok.
+destroy() ->
+    case ets:delete(db) of
+        true -> ok
+    end.
 
 %% db:read(Key, Db) ⇒ {ok, Element} | {error, instance}.
 %-spec read(atom(), db()) -> {ok, atom()} | {error, instance}.
-read(_Key, []) -> {error,instance}; % end of list and no key found
-read(Key, [#data{key=Key, data=Element}| _Rest]) -> {ok, Element}; % matched, return element
-read(Key, [_ | Rest]) -> read(Key, Rest).
+read(Key) -> case ets:lookup(db, Key) of
+                 [] -> {error,instance};
+                 [#data{key=Key, data=Value}] -> {ok, Value}
+             end.
 
 %% db:write(Key, Element, Db) ⇒ NewDb.
 %-spec write(atom(), atom(), db()) -> db().
-write(Key, Element, []) -> [ #data{key=Key, data=Element} ];
-write(Key, Element, [ #data{key=Key} | Rest]) ->
-    [#data{key=Key, data=Element} | Rest];
-write(Key, Element, [ Head | Rest]) ->
-    [Head | write(Key, Element, Rest)].
+write(Key, Element) -> ets:insert(db, #data{key=Key, data=Element}).
 
 %% db:delete(Key, Db) ⇒ NewDb.
 %-spec delete(atom(), db()) -> db().
-delete(_, []) -> [];
-delete(Key, [ #data{key=Key} | Rest]) -> Rest;
-delete(Key, [ Head | Rest]) ->
-    [Head | delete(Key, Rest)].
+delete(Key) -> ets:delete(db, Key).
 
 %% db:match(Element, Db) ⇒ [Key1, ..., KeyN].
 %-spec match(atom(), db()) -> list(atom()).
-match(_, []) -> [];
-match(Element, [#data{key=Key, data=Element} | Rest]) -> [ Key | match(Element, Rest)];
-match(Element, [ _ | Rest]) -> match(Element, Rest).
+match(Element) -> lists:flatten(ets:match(db, #data{key='$1', data=Element})).
 
 -ifdef(TEST).
-new_test() ->
-    ?assertEqual([], new()).
-
-write_to_empty_db_test() ->
-    ?assertEqual([{francesco,london}], write(francesco,london, [])).
-
-write_to_existing_db_test() ->
-    ?assertEqual([{francesco,london}, {lelle, stockholm}],
-                 write(lelle, stockholm, [{francesco,london}])).
-
-read_existing_key_from_db_test() ->
-    ?assertEqual({ok, london}, read(francesco, [{lelle, stockholm},
-                                                {francesco,london}])).
-
-read_missing_key_from_db_test() ->
-    ?assertEqual({error, instance}, read(ola,[{lelle, stockholm},
-                                              {francesco,london}])).
-
-match_multiple_keys_test() ->
-    ?assertEqual([joern, lelle],
-                 match(stockholm,
-                       [{joern,stockholm},{lelle,stockholm},{francesco,london}])).
-
-delete_existing_key_test() ->
-   ?assertEqual([{joern,stockholm},{francesco,london}],
-    delete(lelle, [{joern,stockholm},{lelle,stockholm},{francesco,london}])).
-
-match_after_deleting_key_test() ->
-    ?assertEqual([joern],
-                 match(stockholm, [{joern,stockholm},{francesco,london}])).
 -endif.
 
