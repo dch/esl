a2627280020ec0d5085a8d44f61bc25a06880b20
Dave Cottlehuber <dch@skunkwerks.at>
2014-11-30 14:38:57 +0100

    db: remove all types coz records

--- db/src/db.erl
+++ db/src/db.erl
@@ -5,10 +5,10 @@
 -spec test() -> term().
 -endif.
 
--opaque key() :: {atom(), atom()}.
--opaque db() :: list( key()).
--export_type([key/0, db/0]).
 -record(data, {key, data}).
+%-opaque key() :: {atom(), atom()}.
+%-opaque db() :: list( key()).
+%-export_type([key/0, db/0]).
 -export[ new/0,
          read/2,
          write/3,
@@ -18,15 +18,15 @@
        ].
 
 %% db:new() ⇒ Db.
--spec new() -> db().
+% -spec new() -> db().
 new() -> [].
 
 %% db:destroy(Db) ⇒ ok.
--spec destroy(db()) -> ok.
+% -spec destroy(db()) -> ok.
 destroy(_) -> ok.
 
 %% db:read(Key, Db) ⇒ {ok, Element} | {error, instance}.
--spec read(atom(), db()) -> {ok, atom()} | {error, instance}.
+%-spec read(atom(), db()) -> {ok, atom()} | {error, instance}.
 read(_Key, []) -> {error,instance}; % end of list and no key found
 read(Key, [{Key, Element} | _Rest]) -> {ok, Element}; % matched, return element
 read(Key, [_ | Rest]) -> read(Key, Rest).
@@ -40,14 +40,14 @@ write(Key, Element, [ Head | Rest]) ->
     [Head | write(Key, Element, Rest)].
 
 %% db:delete(Key, Db) ⇒ NewDb.
--spec delete(atom(), db()) -> db().
+%-spec delete(atom(), db()) -> db().
 delete(_, []) -> [];
 delete(Key, [{Key, _ } | Rest]) -> Rest;
 delete(Key, [ Head | Rest]) ->
     [Head | delete(Key, Rest)].
 
 %% db:match(Element, Db) ⇒ [Key1, ..., KeyN].
--spec match(atom(), db()) -> list(atom()).
+%-spec match(atom(), db()) -> list(atom()).
 match(_, []) -> [];
 match(Element, [{Key, Element } | Rest]) -> [ Key | match(Element, Rest)];
 match(Element, [ _ | Rest]) -> match(Element, Rest).
