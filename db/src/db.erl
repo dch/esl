bfae2dc8cbead4c4a3b10ade55b28127d3445c0a
Dave Cottlehuber <dch@skunkwerks.at>
2014-11-30 11:50:50 +0100

    db: add eunit tests

--- db/src/db.erl
+++ db/src/db.erl
@@ -1,8 +1,13 @@
 -module(db).
 
--opaque item() :: {atom(), atom()}.
--opaque db() :: list( item()).
--export_type([item/0, db/0]).
+-ifdef(TEST).
+-include_lib("eunit/include/eunit.hrl").
+-spec test() -> term().
+-endif.
+
+-opaque key() :: {atom(), atom()}.
+-opaque db() :: list( key()).
+-export_type([key/0, db/0]).
 -export[ new/0,
          read/2,
          write/3,
@@ -45,3 +50,37 @@ delete(Key, [ Head | Rest]) ->
 match(_, []) -> [];
 match(Element, [{Key, Element } | Rest]) -> [ Key | match(Element, Rest)];
 match(Element, [ _ | Rest]) -> match(Element, Rest).
+
+-ifdef(TEST).
+new_test() ->
+    ?assertEqual([], new()).
+
+write_to_empty_db_test() ->
+    ?assertEqual([{francesco,london}], write(francesco,london, [])).
+
+write_to_existing_db_test() ->
+    ?assertEqual([{francesco,london}, {lelle, stockholm}],
+                 write(lelle, stockholm, [{francesco,london}])).
+
+read_existing_key_from_db_test() ->
+    ?assertEqual({ok, london}, read(francesco, [{lelle, stockholm},
+                                                {francesco,london}])).
+
+read_missing_key_from_db_test() ->
+    ?assertEqual({error, instance}, read(ola,[{lelle, stockholm},
+                                              {francesco,london}])).
+
+match_multiple_keys_test() ->
+    ?assertEqual([joern, lelle],
+                 match(stockholm,
+                       [{joern,stockholm},{lelle,stockholm},{francesco,london}])).
+
+delete_existing_key_test() ->
+   ?assertEqual([{joern,stockholm},{francesco,london}],
+    delete(lelle, [{joern,stockholm},{lelle,stockholm},{francesco,london}])).
+
+match_after_deleting_key_test() ->
+    ?assertEqual([joern],
+                 match(stockholm, [{joern,stockholm},{francesco,london}])).
+-endif.
+
