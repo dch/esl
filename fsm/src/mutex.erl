e0ee346c92f517cbaa4ffe9486b41f116122ed9b
Dave Cottlehuber <dch@skunkwerks.at>
2014-11-30 17:24:53 +0100

    mutex: trap exits for more fun

--- fsm/src/mutex.erl
+++ fsm/src/mutex.erl
@@ -1,5 +1,5 @@
 -module(mutex).
--export([start/0, wait/0, signal/0, init/0]).
+-export([start/0, wait/0, signal/0, init/0,where/0]).
 
 start() ->
     spawn(?MODULE, init, []).
@@ -17,21 +17,42 @@ wait() ->
         {mutex, Ref, acquired} -> ok
     end.
 
+where() ->
+    case whereis(mutex) of
+        undefined -> start(), timer:sleep(1);
+        _ -> ok
+    end,
+    mutex ! {self(), Ref=make_ref(), where},
+    receive
+        {mutex, Ref, Owner} -> Owner
+    end.
 init() ->
+    process_flag(trap_exit, true),
     register(mutex, self()),
     loop(free).
 
 loop(free) ->
-    Owner = receive {From, Ref, acquire} ->
-                        From ! {mutex, Ref, acquired},
-                        From
-            end,
-    loop({busy, Owner});
+    receive
+        {From, Ref, where} ->
+            From ! {mutex, Ref, free},
+            loop(free);
+        {From, Ref, acquire} ->
+            link(From),
+            From ! {mutex, Ref, acquired},
+            loop({busy, From})
+    end;
 loop({busy, Owner}) ->
-    receive {Owner, Ref, release} ->
-                Owner ! {mutex, Ref, released}
-    end,
-    loop(free).
+    receive
+        {From, Ref, where} ->
+            From ! {mutex, Ref, Owner},
+        loop({busy, Owner});
+        {Owner, Ref, release} ->
+            unlink(Owner),
+            Owner ! {mutex, Ref, released},
+            loop(free);
+        {'EXIT', Owner, _ } ->
+            loop(free)
+    end.
 
 %% mutex:start() ⇒ ok.
 %% mutex:wait() ⇒ ok.
